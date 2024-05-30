65eb4802648f7ba9928a78bf81303caa7b76e7b2
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-05-30 10:01:49 +0200

    add: heading and button for homepage

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,30 @@
-﻿namespace UnitConverter.ViewModels;
+﻿using Avalonia.Media.Imaging;
+using ReactiveUI;
+using System.IO;
 
-public class MainWindowViewModel : ViewModelBase
+namespace UnitConverter.ViewModels
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public Bitmap _logo;
+
+        public string Heading1 => "Currency";
+        public string Heading2 => "Converter";
+        public string CombinedHeading => $"{Heading1}\n{Heading2}";
+        
+        public Bitmap Logo
+        {
+            get => _logo;
+            set => this.RaiseAndSetIfChanged(ref _logo, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            string logoPath = "Assets/images/logo.png";
+            if (File.Exists(logoPath))
+            {
+                Logo = new Bitmap(logoPath);
+            }
+        }
+    }
 }
