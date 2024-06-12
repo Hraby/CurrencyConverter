44716f962c4fe2634f87a77ee3acb1fd4113e0a5
Hraby <hrabyyt@gmail.com>
2024-06-12 12:09:34 +0200

    feat(backend): input. dropdown, binding

--- ViewModels/ConverterPageViewModel.cs
+++ ViewModels/ConverterPageViewModel.cs
@@ -1,6 +1,31 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
 namespace UnitConverter.ViewModels;
 
-public class ConverterPageViewModel : ViewModelBase
+public partial class ConverterPageViewModel : ViewModelBase
 {
-    
+    [ObservableProperty]
+    private string _selectedCurrency;
+
+    public ConverterPageViewModel()
+    {
+        Currencies = new ObservableCollection<string> { "EUR", "USD", "CZK" };
+        SelectedCurrency = Currencies[0];
+    }
+
+    public ObservableCollection<string> Currencies { get; }
+
+    public string FormattedSelectedCurrency => SelectedCurrency switch
+    {
+        "EUR" => "Euro",
+        "USD" => "Americký dolar",
+        "CZK" => "Česká koruna",
+        _ => SelectedCurrency
+    };
+
+    partial void OnSelectedCurrencyChanged(string value)
+    {
+        OnPropertyChanged(nameof(FormattedSelectedCurrency));
+    }
 }
